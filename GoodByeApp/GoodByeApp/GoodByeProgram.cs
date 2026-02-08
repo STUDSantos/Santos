9cf071f79105611d8711633e3b2a1aac7d9bd3e3
STUDSantos <santosjustine09123@gmail.com>
2026-02-08 14:58:34 +0800

    Commit

---
+++ GoodByeApp/GoodByeApp/GoodByeProgram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodByeApp
+{
+    internal class GoodByeProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------ Hello Good Bye World! -----");
+            Console.WriteLine("Press any key to Exit...");
+            Console.ReadKey();
+        }
+    }
+}
