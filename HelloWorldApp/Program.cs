ef3fc46feed05e7a71ffcc72a7066609f8259067
STUDSantos <santosjustine09123@gmail.com>
2026-02-08 14:34:44 +0800

    commit

---
+++ HelloWorldApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldApp
+{
+    internal class HelloWorldProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------ Hello World! -----");
+            Console.WriteLine("Press any key to Exit...");
+            Console.ReadKey();
+        }
+    }
+}
